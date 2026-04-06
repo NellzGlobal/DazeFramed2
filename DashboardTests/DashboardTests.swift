5833c9e25bdb0fe0c604548eea81a7234d5e1209
DuckNellz <shachiorange@gmail.com>
2026-04-06 18:51:06 -0400

    Initial Commit - Personal Dashboard

---
+++ DashboardTests/DashboardTests.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardTests.swift
+//  DashboardTests
+//
+//  Created by Dornell Mister on 4/6/26.
+//
+
+import Testing
+@testable import Dashboard
+
+struct DashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
